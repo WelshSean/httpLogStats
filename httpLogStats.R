2e2e84ecfdae7d80cb962aeade2f93b7119f12fb
Sean Clarke <Sean@Seans-MacBook-Pro.local>
2015-10-06 22:59:14 +0100

    Initial commit

---
+++ httpLogStats.R
@@ -0,0 +1,23 @@
+# Develop a process (using an existing 3rd party tool or your own implementation) that can 
+# consume web access log files and emit the following metrics: 
+#     No. of successful requests per minute 
+#     No. of error requests per minute  
+#     Mean response time per minute  
+#     MBs sent per minute  
+
+# Apache logs in this format
+# "%a %l %u %t \"%r\" %>s %b %D” 
+# %a        IP Address
+# %l        remote log name
+# %u        remote username
+# %t        Time the request was received, in the format [18/Sep/2011:19:18:28 -0400]
+# %r        first line of request
+# %>s       final status
+# %b        size of response in bytes
+# %D        time taken to serve request in microseconds
+
+
+# Lets read in the logs and tidy them up
+
+setwd("/Users/Sean/ss/R")
+logs = read.csv(, sep=" ", header=F)
