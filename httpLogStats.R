3984c14b5e7820baf7532b14e8bd8eac654c5b22
Sean Clarke <Sean@Seans-MacBook-Pro.local>
2015-10-07 00:03:28 +0100

    Basic time manipulation done

--- httpLogStats.R
+++ httpLogStats.R
@@ -18,6 +18,21 @@
 
 
 # Lets read in the logs and tidy them up
+# read.csv with a space for seperator should work fine
+# also add some decent column names
 
 setwd("/Users/Sean/ss/R")
-logs = read.csv(, sep=" ", header=F)
+logs = read.table("access.log", sep=" ", header=F, stringsAsFactors=FALSE, col.names = c("IP", "RLN", "RU", "DATETIME", "GMTOFFSET", "REQ", "STATUS", "SIZE", "SVTIME" ))
+
+# Now lets get the time into R POSIX time format
+
+logs$dt <- strptime(logs$DATETIME, format('[%d/%b/%Y:%H:%M:%S'))
+
+# Calculate earliest time
+starttime <- min(logs$dt)
+
+# Grab date
+logs$date <- format(logs$dt, "15/03/30" )
+
+# Grab minutes since start of interval for buckets
+logs$min <- round(as.numeric(difftime(logs$dt, starttime , unit="mins")))
